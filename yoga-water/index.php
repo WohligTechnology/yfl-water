5d661329cda632db0e6b5d9e4680f6d30425be50
TusharSachde <tushar@wohlig.com>
2016-09-17 19:31:13 +0530

    MOving

---
+++ yoga-water/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Yoga Water | Nourishing Soon</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="shortcut icon" href="img/favicon.png" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+<!-- <link rel="stylesheet" type="text/css" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/mobile.css" />
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,500,700,500italic,700italic' rel='stylesheet' type='text/css'><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<main>
+    <div class="image" style="background-image: url(./img/bg.jpg);height:100vh;width:auto;position:relative;background-size:cover;background-repeat:no-repeat;">
+      <div class="container">
+    <h3 class="logo" style="position:absolute;right:40px;top:40px;"><img src="img/logo.png" class="img-responsive" alt=""></h3>
+    <!-- <p style="text-transform:uppercase; position:absolute;right:40px;top:300px;color: #fff; font-size: 18px;">nourishing soon</p> -->
+    </div>
+  </div>
+</main>
+<footer style="background-color:#000;padding: 10px 0;    position: absolute;
+    width: 100%;
+    bottom: 0;
+    left: 0;">
+  <div class="container">
+  <div class="footer-text text-center">
+    <p style="color:#fff;margin:0;font-size: 12px;"> MADE WITH <img src="img/heart2.png" alt="heart">IN BOMBAY &amp; NEW YORK</p>
+
+
+    <span style="color:#fff;font-size: 12px;">© 2016 <a href="http://yogafoodlove.com/" style="color:#f8cb2a;">YogaFoodLove</a>.</span>
+    <span style="color:#fff;font-size: 12px;">All Rights Reserved.</span>
+  </div>
+
+</div>
+</footer>
+</body>
+</html>
